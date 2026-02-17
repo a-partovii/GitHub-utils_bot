c148da75c96ae541270969e2cd5f265f1acf0b32
Ash <partovi.code@gmail.com>
2026-02-17 13:59:13 +0330

    feat_dev: refactor "get_followers_list" to "extract_usernames", add "following" option

--- src/modules/get_followers_list.py
+++ src/modules/get_followers_list.py
@@ -5,7 +5,7 @@ from modules.delay import delay
 from modules.write_in_file import write_in_file
 from inputs.tokens import secondary_tokens, token_manager, make_headers
 
-def get_followers_list(target_username, output_type="list"):
+def extract_usernames(target_username:str, source:str, output_type:str ="list"):
     """
     Fetch followers of the specified GitHub user.
 
@@ -15,40 +15,43 @@ def get_followers_list(target_username, output_type="list"):
         output_type: "file" to save output, "list" to return as a list (default = list)
     """
     current_date = datetime.now().strftime("%Y-%m-%d_%H;%M") # Get current date and time for the output file naming
-    file_path = f"outputs/({target_username})followers [{current_date}].txt"
+    file_path = f"outputs/({target_username}){source} [{current_date}].txt"
 
     page = 1
-    followers_list = []
+    usernames_list = []
+    if source not in {"followers", "following"}:
+        raise ValueError('Invalid arqument, "source" must be "followers" or following"')
     while True:
         # per_page = 100, max items per request
-        url = f"https://api.github.com/users/{target_username}/followers?per_page=100&page={page}"
+        url = f"https://api.github.com/users/{target_username}/{source}?per_page=100&page={page}"
         headers = make_headers(token_manager(secondary_tokens))
         response = req.get(url, headers=headers)
 
         if response.status_code != 200:
-            print(f"Error fetching followers — HTTP {response.status_code}")
+            print(f"Error: HTTP request failed ({response.status_code}): {response.reason}")
+
             break
 
-        data = response.json()
-        if not data:
+        data_json = response.json()
+        if not data_json:
             break
 
-        for follower in data:
-            followers_list.append(follower.get("login"))
+        for user in data_json:
+            usernames_list.append(user.get("login"))
 
         if output_type == "file": # "file" or "list" (default=List)
             try:
                 os.makedirs("outputs", exist_ok=True) # Create outputs folder if it doesn't exist
-                write_in_file(file_path=file_path, input_item=followers_list, writing_mode="w")
+                write_in_file(file_path=file_path, input_item=usernames_list, writing_mode="w")
 
             except Exception as error:
                 print(f"Writing in the file was not successful, check this error and try again: \n{error}")
 
         page += 1
-        delay(message=f'SUCCESS: "{len(followers_list)}" usernames saved to "{file_path}"') # Random delay per request (2–6 seconds)
+        delay(message=f'SUCCESS: "{len(usernames_list)}" usernames saved to "{file_path}"') # Random delay per request (2–6 seconds)
               
     if output_type == "file":
-        print(f'SUCCESS: Extracting usernames is done, "{len(followers_list)}" usernames saved to "{file_path}"')
+        print(f'SUCCESS: Extracting usernames is done, "{len(usernames_list)}" usernames saved to "{file_path}"')
     else:
-        print(f'SUCCESS: Extracting usernames is done, "{len(followers_list)}" usernames saved.')
-    return followers_list
+        print(f'SUCCESS: Extracting usernames is done, "{len(usernames_list)}" usernames saved.')
+    return usernames_list

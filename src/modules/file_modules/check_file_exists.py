b4a82e4ab670b80ba1eb95c831c02ffb749c561d
Ash <partovi.code@gmail.com>
2026-02-18 22:21:57 +0330

    chore: rename [`check_file_exists.py`]

--- src/modules/file_modules/check_file_exists.py
+++ src/modules/file_modules/check_file_exists.py
@@ -5,6 +5,6 @@ def check_file_exists(file_path:str) -> bool:
     Check if a file exists at the specified path.
 
     Args:
-        file_path (str): The path to the file to check.
+        file_path (str): The path to the file (with slash and extension).
     """         
     return os.path.exists(file_path)

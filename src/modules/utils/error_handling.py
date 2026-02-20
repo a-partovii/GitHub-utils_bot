b5ee7e2d10026ed865890bcb75f5fd83971fccb2
Ash <partovi.code@gmail.com>
2026-02-20 23:56:19 +0330

    feat_add: add "response_error_handler" to [`error_handling.py`]

--- src/modules/utils/error_handling.py
+++ src/modules/utils/error_handling.py
@@ -11,3 +11,36 @@ def network_error_handler(error):
     """
     if error is not None:
         return f"[ERROR] Network Error ({type(error).__name__}): {error}"
+
+def response_error_handler(response):
+    """
+    Handles GitHub HTTP response errors only.
+    Does nothing for successful responses.
+    """
+    if response is None:
+        return
+
+    # Response handling
+    status = response.status_code
+    remaining = response.headers.get("X-RateLimit-Remaining", "unknown")
+
+    # Token error
+    if status == 401:
+        return "[ERROR] Invalid or Expired Token."
+
+    # Rate limit reached
+    elif status == 403 or (remaining != "unknown" and int(remaining) <= 5):
+        return (f'[WARN] Token RateLimit is Close to Being Reached, Remaining: "{remaining}"' +
+                         "\n Take a break and continue later...")
+
+    # Not found
+    elif status == 404:
+       return "[WARN] Resource not found"
+
+    # GitHub internal error (server)
+    elif status >= 500:
+        return f"[ERROR] Server error {status}"
+
+    # other
+    elif status >= 400:
+        return f"[ERROR] http= {status}, reason: {response.reason}"

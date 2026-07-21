5458863ecceda2fb9a036affeb5e669bdd3d9362
Ash <partovi.code@gmail.com>
2026-07-21 22:26:53 +0330

    dev_add: 'ask_output_type' func to [`extract_oprs.py`]

--- src/modules/operations/extract _oprs.py
+++ src/modules/operations/extract _oprs.py
@@ -1,3 +1,24 @@
+def ask_output_type():
+    print("How would you like to have the result?")
+    print("1. Display in the terminal (default)")
+    print("2. Save to an output file")
+    print("3. Display in the terminal and save a output file")
+    print()
+
+    try:
+        user_input = int(input("Select an option: ")) or 1
+    except ValueError:
+            print("Invalid input! The default value will be replaced.")
+            user_input = 1
+
+    if user_input == 1 :
+        return [1, "list"]
+    
+    elif user_input == 2:
+       return [2, "file"]
+    
+    return [3, "file"]
+
 extract_submenu = {
     # "1": {"label": "Extract your followers usernames", "action": extract_your_followers},
     # "2": {"label": "Extract your following usernames", "action": extract_your_following},

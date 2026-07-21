b74703ba5dfd0aad0cbfaf329ffdef44375e3356
Ash <partovi.code@gmail.com>
2026-07-21 22:15:39 +0330

    feat_add: new option in "extract_submenue"

--- src/modules/operations/extract _oprs.py
+++ src/modules/operations/extract _oprs.py
@@ -1,11 +1,12 @@
 extract_submenu = {
     # "1": {"label": "Extract your followers usernames", "action": extract_your_followers},
     # "2": {"label": "Extract your following usernames", "action": extract_your_following},
-    # "3": {"label": "Extract a user's followers", "action": extract_from_followers},
-    # "4": {"label": "Extract a user's following", "action": extract_from_following},
-    # "5": {"label": "Extract usernames of users who starred your repositories", "action": extract_my_stargazers},
-    # "6": {"label": "Extract usernames of users who starred a given user's repositories", "action": extract_user_stargazers},
-    # "7": {"label": "Extract usernames of users who starred a given repository", "action": extract_repo_stargazers},
-    # "8": {"label": "Extract bulk usernames to follow with a limit", "action": extract_bulk},
+    # "3": {"label": "Extract usernames you follow who don't follow you back", "action": extract_non_followers},
+    # "4": {"label": "Extract a user's followers", "action": extract_from_followers},
+    # "5": {"label": "Extract a user's following", "action": extract_from_following},
+    # "6": {"label": "Extract usernames of users who starred your repositories", "action": extract_my_stargazers},
+    # "7": {"label": "Extract usernames of users who starred a given user's repositories", "action": extract_user_stargazers},
+    # "8": {"label": "Extract usernames of users who starred a given repository", "action": extract_repo_stargazers},
+    # "9": {"label": "Extract bulk usernames to follow with a limit", "action": extract_bulk},
     # "0": {"label": "Back to main menu", "action": return_to_main},
 }

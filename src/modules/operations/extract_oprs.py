c58e89d38ed252b94f95a1a60c49f77a98f997bf
Ash <partovi.code@gmail.com>
2026-07-23 19:43:51 +0330

    feat_add: 'extract_non_follower_following' func

--- src/modules/operations/extract_oprs.py
+++ src/modules/operations/extract_oprs.py
@@ -1,5 +1,6 @@
 from modules.major_modules import extract_usernames
-from modules.file_modules import read_file
+from modules.file_modules import read_file, write_file, filename_datetime
+from modules.utils import filter_list
 from config.tokens import primary_token
 from modules.tui import print_in_columns, ask_output_type
 
@@ -10,7 +11,7 @@ def extract_your_followers():
         my_followers = extract_usernames(
              target_username=my_username,
              source="followers",
-             output_type= output_type[1]
+             output_type=output_type[1]
         )
 
         if output_type[0] == 1 :
@@ -37,10 +38,36 @@ def extract_your_following():
               print_in_columns(read_file(my_following))
 
 # -----------------------------------------------------------------------------------------
+def extract_non_follower_following():
+    output_type = ask_output_type()
+    my_username = next(iter(primary_token))
+    try:
+        my_following = extract_usernames(
+                target_username=my_username,
+                source="following",
+        )
+        my_followers = extract_usernames(
+                target_username=my_username,
+                source="followers",
+        )
+
+        if output_type[0] == 2 or output_type[0] == 3 :
+            file_path = f"outputs/({my_username})non-follower-following {filename_datetime()}"
+            write_file(file_path, filter_list(my_following, my_followers))
+            print(f'[SUCCESS] Extracting Usernames is Done, usernames saved to "{file_path}"\n')
+
+        if output_type[0] == 1 or output_type[0] == 3 :
+            print("Usernames you follow who don't follow you back:")
+            print_in_columns(filter_list(my_following, my_followers))
+
+    except Exception as error:
+        print(f"[ERROR] {error}")
+        
+# -----------------------------------------------------------------------------------------
 extract_submenu = {
     "1": {"label": "Extract your followers usernames", "action": extract_your_followers},
     "2": {"label": "Extract your following usernames", "action": extract_your_following},
-    # "3": {"label": "Extract usernames you follow who don't follow you back", "action": extract_non_followers},
+    "3": {"label": "Extract usernames you follow who don't follow you back", "action": extract_non_follower_following},
     # "4": {"label": "Extract a user's followers", "action": extract_from_followers},
     # "5": {"label": "Extract a user's following", "action": extract_from_following},
     # "6": {"label": "Extract usernames of users who starred your repositories", "action": extract_my_stargazers},

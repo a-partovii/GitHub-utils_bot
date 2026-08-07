51fd614e6600833dee965def56a931db498fcb1d
Ash <partovi.code@gmail.com>
2026-08-07 19:24:02 +0330

    feat_newFile: add [`configuration_oprs.py`], includes "config_submenu"

---
+++ src/modules/operations/configuration_oprs.py
@@ -0,0 +1,7 @@
+config_submenu = {
+    "1": {"label": "Adjust Blacklist", "action": ""},
+    "2": {"label": "Adjust Whitelist", "action": ""},
+    "3": {"label": "Adjust Greylist", "action": ""},
+    "4": {"label": "Manage GitHub tokens", "action": ""}
+    # "0": {"label": "Back to main menu", "action": return_to_main},
+}

06e01fecdbe94f9afdc70a6513f96b49d4c9e46c
Ash <partovi.code@gmail.com>
2026-08-09 19:13:07 +0330

    feat_add: 'adjust_files_menu' func to [`configuration_oprs.py`]

--- src/modules/operations/configuration_oprs.py
+++ src/modules/operations/configuration_oprs.py
@@ -1,3 +1,14 @@
+def adjust_files_menu(list_name:str, file_path:str):
+    _ = {
+    "1": {"label": f"Show {list_name}", "action": lambda: print_in_columns(read_file(file_path))},
+    "2": {"label": f"Add new usernames to {list_name}", "action": lambda:""},
+    "3": {"label": f"Remove usernames from {list_name}", "action": lambda:""},
+    "4": {"label": f"Reset {list_name}", "action": lambda: ""},
+    "0": {"label": "Exit menu", "action": ""}
+}
+    show_menu(_)
+
+# ---------------------------------------------------------------------
 config_submenu = {
     "1": {"label": "Adjust Blacklist", "action": ""},
     "2": {"label": "Adjust Whitelist", "action": ""},
